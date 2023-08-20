08460a526b1715cbf12c631957bd81f5007b2834
1292852402 <1292852402@FV-SHARIFI-665>
2023-08-20 11:56:32 +0330

    Add project files.

---
+++ Currency/Controllers/CurrencyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Currency.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+    }
+}
